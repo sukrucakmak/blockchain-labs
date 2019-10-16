a2fe8185f9108c392247233ea99c80fd702de963
sukru.cakmak <sukru@sukru.org>
2019-10-16 15:02:38 +0300

    Description added

--- cryptographic-hash/src/main/java/org/sukru/cryptographichash/App.java
+++ cryptographic-hash/src/main/java/org/sukru/cryptographichash/App.java
@@ -8,12 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// one-way
         String message1 = "Hello from Sukru!";
         System.out.println("Hash Code of '"+ message1 +"': "+ message1.hashCode());
         
+        // deterministic
+        System.out.println("Hash Code of '"+ message1 +"': "+ message1.hashCode());
+        
+        // fixed size
         String message2 = "Hello from Sükrü!";
         System.out.println("Hash Code of '"+ message2 +"': "+ message2.hashCode());
         
+        // pseudo random
         String message3 = "Hello from Şükrü!";
         System.out.println("Hash Code of '"+ message3 +"': "+ message3.hashCode());
     }
